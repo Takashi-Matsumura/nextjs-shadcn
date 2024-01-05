7b4f42d11b1695414d832a25100f8044b778698a
Takashi-Matsumura <mats@coral.plala.or.jp>
2024-01-05 21:13:19 +0900

    Add drawer and dialog components to employee datatable columns

--- src/app/ui/employee/datatable-columns.tsx
+++ src/app/ui/employee/datatable-columns.tsx
@@ -1,7 +1,36 @@
 import { ColumnDef } from "@tanstack/react-table";
 
 import { Employee } from "./data/schema";
-import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import {
+  PencilIcon,
+  TrashIcon,
+  PlusIcon,
+  DocumentPlusIcon,
+} from "@heroicons/react/24/outline";
+
+import { Button } from "@/components/ui/button";
+import { EmployeeForm } from "./employeeForm";
 
 export const columns: ColumnDef<Employee>[] = [
   {
@@ -51,7 +80,44 @@ export const columns: ColumnDef<Employee>[] = [
     cell: ({ row }) => {
       const customer = row.original;
 
-      return <div className="space-x-2"></div>;
+      return (
+        <div className="space-x-2">
+          <Drawer>
+            <DrawerTrigger asChild>
+              <Button variant="outline">
+                <PencilIcon className="h-4 w-4" />
+              </Button>
+            </DrawerTrigger>
+            <DrawerContent>
+              <DrawerHeader className="text-left">
+                <DrawerTitle>Edit profile</DrawerTitle>
+                <DrawerDescription>
+                  Make changes to your profile here. Click save when you're
+                  done.
+                </DrawerDescription>
+              </DrawerHeader>
+              <EmployeeForm className="px-4 grid items-start gap-4" />
+            </DrawerContent>
+          </Drawer>
+
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button variant="outline">
+                <TrashIcon className="h-4 w-4" />
+              </Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Are you sure absolutely sure?</DialogTitle>
+                <DialogDescription>
+                  This action cannot be undone. This will permanently delete
+                  your account and remove your data from our servers.
+                </DialogDescription>
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
+        </div>
+      );
     },
   },
 ];
---
+++ src/app/ui/employee/employeeForm.tsx
@@ -0,0 +1,86 @@
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { DrawerClose } from "@/components/ui/drawer";
+
+type EmployeeFormProps = {
+  className?: string;
+  // 他のpropsもここに追加します
+};
+
+export function EmployeeForm({ className }: EmployeeFormProps) {
+  return (
+    <form className="p-10">
+      <div className="grid grid-cols-3 gap-10">
+        <div>
+          <div>
+            <Label htmlFor="kana">ふりがな</Label>
+            <Input id="kana" placeholder="やまだたろう" defaultValue="" />
+          </div>
+          <div>
+            <Label htmlFor="name">名前</Label>
+            <Input id="name" placeholder="山田太郎" defaultValue="" />
+          </div>
+        </div>
+        <div>
+          <div>
+            <Label htmlFor="gender">性別</Label>
+            <RadioGroup className="flex items-center justify-start space-x-5 mb-4">
+              <div>
+                <RadioGroupItem value="male" id="male"></RadioGroupItem>
+                <Label htmlFor="male">男性</Label>
+              </div>
+              <div>
+                <RadioGroupItem value="female" id="female"></RadioGroupItem>
+                <Label htmlFor="female">女性</Label>
+              </div>
+            </RadioGroup>
+          </div>
+          <div>
+            <Label htmlFor="birthdate">誕生日</Label>
+            <Input
+              id="birthdate"
+              placeholder="仮に配置しています"
+              defaultValue=""
+            />
+          </div>
+        </div>
+        <div>
+          <div>
+            <Label htmlFor="phone">電話番号</Label>
+            <Input id="phone" placeholder="000-000-0000" defaultValue="" />
+          </div>
+          <div>
+            <Label htmlFor="mail">メール</Label>
+            <Input id="mail" placeholder="mail@domain" defaultValue="" />
+          </div>
+        </div>
+      </div>
+      <div className="grid grid-cols-2 gap-10">
+        <div>
+          <div>
+            <Label htmlFor="postal">郵便番号</Label>
+            <Input id="postal" placeholder="000-0000" defaultValue="" />
+          </div>
+          <div>
+            <Label htmlFor="address">住所</Label>
+            <Input id="address" placeholder="市町村名" defaultValue="" />
+          </div>
+        </div>
+        <div>
+          <Label htmlFor="notes">特記事項</Label>
+          <Textarea id="notes" placeholder="" defaultValue="" />
+        </div>
+      </div>
+
+      <div className="pt-10 flex items-center justify-between">
+        <Button type="submit">Save changes</Button>
+        <DrawerClose asChild>
+          <Button variant="outline">Cancel</Button>
+        </DrawerClose>
+      </div>
+    </form>
+  );
+}
