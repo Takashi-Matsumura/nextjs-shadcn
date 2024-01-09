1ad79738fd7c163b337f06c8001b301d29bfc73f
Takashi-Matsumura <mats@coral.plala.or.jp>
2024-01-09 09:44:40 +0900

    Update font imports in layout.tsx

--- src/app/ui/fonts.ts
+++ src/app/ui/fonts.ts
@@ -1,3 +1,12 @@
 import { Inter } from "next/font/google";
+import { Noto_Sans_JP } from "next/font/google";
+
+// 参考URL https://fonts.google.com/variablefonts
+
+export const noto = Noto_Sans_JP({
+  weight: ["400", "700"],
+  style: "normal",
+  subsets: ["latin"],
+});
 
 export const inter = Inter({ subsets: ["latin"] });
