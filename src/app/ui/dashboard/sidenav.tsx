dbd4574192a0b2b3caa8b4999baeba9259c4d287
Takashi-Matsumura <mats@coral.plala.or.jp>
2024-01-10 02:42:55 +0900

    Refactor SideNav component and add logo and link

--- src/app/ui/dashboard/sidenav.tsx
+++ src/app/ui/dashboard/sidenav.tsx
@@ -1,25 +1,35 @@
 import { PowerIcon } from "@heroicons/react/24/outline";
 import NavLinks from "./nav-links";
 import { signOut } from "@/lib/authentication/auth";
+import MyLogo from "@/app/ui/logo";
+import Link from "next/link";
 
 export default function SideNav() {
   return (
-    <div className="flex h-full flex-row flex-wrap space-x-4 md:space-x-0 px-3 py-4 md:px-2 md:justify-between">
-      <div className="flex grow flex-row space-x-2 md:flex-col md:space-x-0 md:space-y-2">
-        <NavLinks />
-      </div>
-      <form
-        action={async () => {
-          "use server";
-          await signOut();
-        }}
-        className="mt-auto md:w-full"
+    <div className="flex h-full flex-col px-3 py-4 md:px-2">
+      <Link
+        className="mb-2 flex h-10 items-end justify-start rounded-md bg-blue-600 p-4 md:h-20"
+        href="/"
       >
-        <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
-          <PowerIcon className="w-6" />
-          <div className="hidden md:block">Sign Out</div>
-        </button>
-      </form>
+        <div className="w-32 text-white md:w-40">
+          <MyLogo />
+        </div>
+      </Link>
+      <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+        <NavLinks />
+        <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
+        <form
+          action={async () => {
+            "use server";
+            await signOut();
+          }}
+        >
+          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+            <PowerIcon className="w-6" />
+            <div className="hidden md:block">Sign Out</div>
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
---
+++ src/app/ui/logo.tsx
@@ -0,0 +1,10 @@
+import { HeartIcon } from "@heroicons/react/24/outline";
+
+export default function MyLogo() {
+  return (
+    <div className="flex flex-row items-center leading-none text-white">
+      <HeartIcon className="hidden md:block h-12 w-12 rotate-[15deg]" />
+      <p className="text-xs md:block text-[30px] pl-3">Sample</p>
+    </div>
+  );
+}
