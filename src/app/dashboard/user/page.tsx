547262a26fd1f6037478d7da9226bce66d348ff4
Takashi-Matsumura <mats@coral.plala.or.jp>
2024-01-16 20:49:02 +0900

    Remove unused imports and code

--- src/app/dashboard/user/page.tsx
+++ src/app/dashboard/user/page.tsx
@@ -1,9 +1,4 @@
-// "use client";
-
-import { DataTableQuery } from "@/app/ui/user/datatable-query";
 import { fetchRevenue, fetchUsers } from "@/app/lib/data";
-import { useEffect, useState } from "react";
-import { User } from "@/app/lib/definitions";
 
 export default async function Page() {
   const users = await fetchUsers();
